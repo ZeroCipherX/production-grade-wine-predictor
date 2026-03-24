0becbb4f3290da43398e4e803d40ee4dade34a65
ZeroCipherX <saqibkhank805@gmail.com>
2026-03-24 17:06:06 +0500

    Logging Functionality

---
+++ main.py
@@ -0,0 +1,2 @@
+from src.productiongradewinepredictor import logger
+logger.info("Testing the logger from main.py")
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name="production-grade-wine-predictor"
+project_name="productiongradewinepredictor"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
