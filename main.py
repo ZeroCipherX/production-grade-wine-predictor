4f59b9f57dc99e727fc82fe1cbb3fc838e91fd0f
ZeroCipherX <saqibkhank805@gmail.com>
2026-03-25 02:44:46 +0500

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.productiongradewinepredictor import logger
 from src.productiongradewinepredictor.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.productiongradewinepredictor.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.productiongradewinepredictor.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -18,6 +18,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
