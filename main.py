cf6e2106596ee4027ce921f6666013e3af84936f
ZeroCipherX <saqibkhank805@gmail.com>
2026-03-25 03:29:13 +0500

    Model Trainer Implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.productiongradewinepredictor import logger
 from src.productiongradewinepredictor.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.productiongradewinepredictor.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.productiongradewinepredictor.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.productiongradewinepredictor.pipeline.model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -29,6 +30,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
